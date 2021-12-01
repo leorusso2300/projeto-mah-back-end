a156ffaecb19ad79835b01009748c3e70c870e28
leorusso2300 <leorusso2607@gmail.com>
2021-12-01 10:01:50 -0300

    versão final

---
+++ src/test/java/com/generation/mah/MahApplicationTests.java
@@ -0,0 +1,13 @@
+package com.generation.mah;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MahApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
